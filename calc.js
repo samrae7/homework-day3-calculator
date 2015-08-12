c09988da2299c41fe4c735d8458a828de025e76e
Sam Rae <samuelrae@gmail.com>
2015-08-12 22:48:47 +0100

    fixed mortgage calculator which was not converting interest rate to monthly rate

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-var mode = prompt ('Choose a calculator mode: N - normal / M- Mortage Calculator / B - BMI calculator / T - Trip Calculotor');
+var mode = prompt ('Choose a calculator mode: N - normal / M- Mortage Calculator');
   console.log('mode', mode);
 
 if (mode=='N') {
@@ -8,50 +8,59 @@ if (mode=='N') {
   var num1 = prompt('choose a number');
   console.log('num1', num1);
 
-  if (op!=='sqRoot') {
+  var goodNum1 = parseFloat(num1);
+  console.log('goodNum1',goodNum1);
+
+    if (isNaN(goodNum1)==true) {
+      alert('Please reload the page and enter a valid number');
+    }
+
+    else if (op!=='sqRoot') {
     var num2 = prompt('choose the second number');
     console.log('num2', num2);
 
     var goodNum2 = parseFloat(num2);
     console.log('goodNum2',goodNum2);
+
+      if (isNaN(parseFloat(num2))==true) {
+          alert('Please reload the page and enter a valid number');
+      };
+    
+    };
+
+    var result;
+
+    if (op=="add") {
+      result = goodNum1+goodNum2;
     }
 
-  var goodNum1 = parseFloat(num1);
-  console.log('goodNum1',goodNum1);
-
-  var result;
-
-  if (op=="add") {
-    result = goodNum1+goodNum2;
-  }
-
-  else if (op=='divide') {
+    else if (op=='divide') {
     result = goodNum1 / goodNum2;
 
-  }else if (op=='subtract') {
+  } else if (op=='subtract') {
     result = goodNum1 - goodNum2;
 
-  }else if (op=='multiply') {
+  } else if (op=='multiply') {
     result = goodNum1 * goodNum2;
   }
-  else if (op=='sqRoot') {
+    else if (op=='sqRoot') {
     result =Math.sqrt(goodNum1);
   }
-  else if (op=='power') {
+    else if (op=='power') {
     result = Math.pow(num1,num2);
   }
 
   console.log('result', result);
-  alert('The result is xxx'+result);
+  alert('The result is '+result);
 }
- else if (mode='M') {
+else if (mode=='M') {
   var principal = prompt ("Enter principal:");
   console.log('principal', principal); 
   var principalNo = parseFloat(principal); 
 
   var interestRate = prompt ("Enter interest rate:");
   console.log('interestRate', interestRate);
-  var interestRateNo = parseFloat(interestRate);
+  var interestRateNo = parseFloat(interestRate)/(100*12);
 
   var noPayments = prompt ("Enter number of payments:");
   console.log('noPayments', noPayments);
@@ -60,5 +69,7 @@ if (mode=='N') {
   var monthlyPayment = (((Math.pow((1+interestRateNo),noPaymentsNo))*interestRateNo)/((Math.pow((1+interestRateNo),noPaymentsNo))-1))*principalNo;
 
   console.log('monthlyPayment', monthlyPayment);
-  alert('Your monthly payment '+monthlyPayment);
+  alert('Your monthly payment is '+monthlyPayment.toFixed(2));
+ } else {
+  alert('Please reload the page and choose a valid mode');
  }
