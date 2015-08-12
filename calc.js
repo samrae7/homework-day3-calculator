3b2cf8acc1f5553090390732ac4d7fd6de30d8be
Sam Rae <samuelrae@gmail.com>
2015-08-12 21:38:54 +0100

    I got confused with previous repository as I changed my folder structure after I had created the remote repository. So this is the first commit. I have made the advanced calcultor and now trying to make the mortgage calculator work

--- calc.js
+++ calc.js
@@ -1,6 +1,62 @@
-var op =prompt ('choose an operation');
-console.log('op', op);
-var num = prompt('choose a number');
-console.log('num', num);
+var mode = prompt ('Choose a calculator mode: N - normal / M- Mortage Calculator / B - BMI calculator / T - Trip Calculotor');
+  console.log('mode', mode);
 
-alert('Why is your console not open')
+if (mode=='N') {
+  var op = prompt ("choose an operation: 'add', 'subtract', 'multiply' or 'divide'");
+  console.log('op', op);
+
+  var num1 = prompt('choose a number');
+  console.log('num1', num1);
+
+  if (op!=='sqRoot') {
+    var num2 = prompt('choose the second number');
+    console.log('num2', num2);
+
+    var goodNum2 = parseFloat(num2);
+    console.log('goodNum2',goodNum2);
+    }
+
+  var goodNum1 = parseFloat(num1);
+  console.log('goodNum1',goodNum1);
+
+  var result;
+
+  if (op=="add") {
+    result = goodNum1+goodNum2;
+  }
+
+  else if (op=='divide') {
+    result = goodNum1 / goodNum2;
+
+  }else if (op=='subtract') {
+    result = goodNum1 - goodNum2;
+
+  }else if (op=='multiply') {
+    result = goodNum1 * goodNum2;
+  }
+  else if (op=='sqRoot') {
+    result =Math.sqrt(goodNum1);
+  }
+  else if (op=='power') {
+    result = Math.pow(num1,num2);
+  }
+
+  console.log('result', result);
+  alert('The result is xxx'+result);
+}
+ else if (mode='M') {
+  var principal = prompt ("Enter principal:");
+  console.log('principal', principal); 
+  var Nprincipal = parseFloat 
+
+  var interestRate = prompt ("Enter interest rate:");
+  console.log('interestRate', interestRate);
+
+  var noPayments = prompt ("Enter number of payments:");
+  console.log('noPayments', noPayments); 
+
+  var monthlyPayment = (((Math.pow((1+interestRate),noPayments))*interestRate)/((Math.pow((1+interestRate),noPayments))-1))*principal;
+
+  console.log('monthlyPayment', monthlyPayment);
+  alert('Your monthly payment '+monthlyPayment);
+ }
